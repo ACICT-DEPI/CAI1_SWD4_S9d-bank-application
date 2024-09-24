d69c797cd57bb6ef14be1fae28c0dedebe517d65
Aalaa0701 <Alaaalaaashour1507@gmail.com>
2024-09-24 15:50:40 +0300

    Language screen design complete

--- lib/screen/settings/language_profile.dart
+++ lib/screen/settings/language_profile.dart
@@ -11,12 +11,73 @@ class LanguageProfileScreen extends StatelessWidget {
         Get.put(LanguagePageControllerImpl());
     return Scaffold(
       appBar: AppBar(
-        title: Text('Language'),
+        leading: IconButton(
+            onPressed: (){Navigator.pop(context);},
+            icon: Icon(Icons.arrow_back_ios_new_rounded)
+        ),
+        title: Text('Language', style: TextStyle(fontWeight: FontWeight.bold),),
         elevation: 0,
-        centerTitle: true,
       ),
-      body: Center(
-        child: Text('Language Screen'),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                width: double.infinity,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.grey.shade300)
+                    )
+                ),
+                child: TextButton(
+                    onPressed: () {
+                      //edit app's language
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset('assets/images/eg.png'),
+                        SizedBox(width: 25,),
+                        Text(
+                          'Arabic',
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                        SizedBox(width: 200,),
+                        Icon(Icons.arrow_forward_ios_rounded, size: 16, color: Colors.grey.shade300)
+                      ],
+                    )),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                width: double.infinity,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.grey.shade300)
+                    )
+                ),
+                child: TextButton(
+                    onPressed: () {
+                      //edit app's language
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset('assets/images/us.png'),
+                        SizedBox(width: 25,),
+                        Text(
+                          'English',
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                        SizedBox(width: 195,),
+                        Icon(Icons.arrow_forward_ios_rounded, size: 16, color: Colors.grey.shade300)
+                      ],
+                    )),
+              ),
+            ],
+          )
       ),
     );
   }
