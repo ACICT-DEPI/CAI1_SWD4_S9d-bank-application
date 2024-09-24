f7dc037fcca4b48067253e29d6d7eb1dbf882eb0
Aalaa0701 <Alaaalaaashour1507@gmail.com>
2024-09-24 20:17:25 +0300

    small modification in edit profile screen

--- lib/screen/settings/edit_profile.dart
+++ lib/screen/settings/edit_profile.dart
@@ -25,91 +25,53 @@ class EditProfileScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: (){//pick image
-                 },
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  CircleAvatar(
-                    radius: 60,
-                  ),
-                  CircleAvatar(
-                    radius: 15,
-                    backgroundColor: AppColor.primaryColor,
-                    child: Icon(Icons.add, color: Colors.white,),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              padding: EdgeInsets.only(top: 30),
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(color: Colors.grey.shade400)
-                )
-              ),
-              child: Center(),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 10, 8, 15),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                    "Name",
-                    style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.7),),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: (){//pick image
+                   },
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                    ),
+                    CircleAvatar(
+                      radius: 15,
+                      backgroundColor: AppColor.primaryColor,
+                      child: Icon(Icons.add, color: Colors.white,),
                     )
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: TextFormField(
-                      controller: nameController,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.normal,
-                          overflow: TextOverflow.ellipsis
-                      ),
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
-                        ),
-                        filled: true,
-                        fillColor: AppColor.primaryColor.withOpacity(0.2),
-                      ),
-
-                    )
-                  )
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 10, 8, 15),
-              child: Row(
-                children: [
-                  Expanded(
+              SizedBox(height: 20,),
+              Container(
+                padding: EdgeInsets.only(top: 30),
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.grey.shade400)
+                  )
+                ),
+                child: Center(),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 10, 8, 15),
+                child: Row(
+                  children: [
+                    Expanded(
                       flex: 2,
                       child: Text(
-                        "Phone",
-                        style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.7),),
+                      "Name",
+                      style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.7),),
                       )
-                  ),
-                  Expanded(
+                    ),
+                    Expanded(
                       flex: 3,
                       child: TextFormField(
-                        controller: phoneController,
-                        keyboardType: TextInputType.phone,
+                        controller: nameController,
                         style: TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.normal,
@@ -121,76 +83,116 @@ class EditProfileScreen extends StatelessWidget {
                             borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
                           ),
                           focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                              borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
-                          ),
-                          filled: true,
-                          fillColor: AppColor.primaryColor.withOpacity(0.2),
-                        ),
-
-                      )
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 10, 8, 40),
-              child: Row(
-                children: [
-                  Expanded(
-                      flex: 2,
-                      child: Text(
-                        "Email",
-                        style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.7),),
-                      )
-                  ),
-                  Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: emailController,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.normal,
-                            overflow: TextOverflow.ellipsis
-                        ),
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(30.0),
-                              borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                              borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
+                            borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
                           ),
                           filled: true,
                           fillColor: AppColor.primaryColor.withOpacity(0.2),
                         ),
-
+        
                       )
-                  )
-                ],
-              ),
-            ),
-
-            Container(
-              alignment: Alignment.centerRight,
-              child: Container(
-                width: screenWidth/2,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AppColor.primaryColor
-                ),
-                child: TextButton(
-                    onPressed: (){
-                      //save logic
-                      controller.gotToProfileInformation();
-                    },
-                    child: Text("Save update", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 10, 8, 15),
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Phone",
+                          style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.7),),
+                        )
+                    ),
+                    Expanded(
+                        flex: 3,
+                        child: TextFormField(
+                          controller: phoneController,
+                          keyboardType: TextInputType.phone,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                              overflow: TextOverflow.ellipsis
+                          ),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                                borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
+                            ),
+                            filled: true,
+                            fillColor: AppColor.primaryColor.withOpacity(0.2),
+                          ),
+        
+                        )
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 10, 8, 40),
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Email",
+                          style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.7),),
+                        )
+                    ),
+                    Expanded(
+                        flex: 3,
+                        child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: emailController,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                              overflow: TextOverflow.ellipsis
+                          ),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                                borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                                borderSide: BorderSide(color: AppColor.primaryColor.withOpacity(0))
+                            ),
+                            filled: true,
+                            fillColor: AppColor.primaryColor.withOpacity(0.2),
+                          ),
+        
+                        )
+                    )
+                  ],
+                ),
+              ),
+        
+              Container(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: screenWidth/2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: AppColor.primaryColor
+                  ),
+                  child: TextButton(
+                      onPressed: (){
+                        //save logic
+                        controller.gotToProfileInformation();
+                      },
+                      child: Text("Save update", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
