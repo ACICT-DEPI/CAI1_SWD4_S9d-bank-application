7424f06701d58bba93ec3b3649dd48129d9de21d
ahmed-sala <ahmeds66210@gmail.com>
2024-09-26 17:14:04 +0300

    Finish home design.

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,25 +1,71 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
 import 'package:get/get.dart';
 import 'package:vaulta/core/services/sevices.dart';
 
 import '../core/constant/routes.dart';
 
 abstract class HomeController extends GetxController {
-  signOut();
   goToTransferScreen();
+  goToTransactionScreen();
+  goToWithdrawScreen();
+  goToAccountScreen();
 }
 
 class HomeControllerImpl extends HomeController {
   MyServices myServices = Get.find();
-  @override
-  signOut() async {
-    await FirebaseAuth.instance.signOut();
-    myServices.sharedPreferences.remove('userid');
-    Get.offNamed(AppRoute.login);
+
+  RxString username = ''.obs;
+  RxString imagePath = ''.obs; // For user image
+  late String userId;
+  late FirebaseFirestore firestore;
+  final FirebaseAuth auth = FirebaseAuth.instance; // Initialize Firebase Auth
+
+  fetchUsername() async {
+    final user = await firestore.collection('users').doc(userId).get();
+    username.value = user['username'];
+    fetchUserImage(); // Fetch the user image based on Firebase Auth user
+  }
+
+  fetchUserImage() {
+    User? currentUser = auth.currentUser; // Get the current user
+    if (currentUser != null) {
+      // Check if the user has a photo URL
+      if (currentUser.photoURL != null) {
+        imagePath.value = currentUser.photoURL!; // Fetch image URL if available
+      } else {
+        imagePath.value = ''; // Set to empty if no photo URL
+      }
+    } else {
+      imagePath.value = ''; // Set to empty if no user is logged in
+    }
   }
 
   @override
   goToTransferScreen() {
     Get.offNamed(AppRoute.transfer);
+  }
+
+  @override
+  goToAccountScreen() {
+    throw UnimplementedError();
+  }
+
+  @override
+  goToTransactionScreen() {
+    Get.offNamed(AppRoute.transactionReport);
+  }
+
+  @override
+  goToWithdrawScreen() {
+    Get.offNamed(AppRoute.withdrawScreen);
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    userId = myServices.sharedPreferences.getString('userid')!;
+    firestore = FirebaseFirestore.instance;
+    fetchUsername();
   }
 }
---
+++ lib/widget/home/home_gridview.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vaulta/widget/home/grid_view_item.dart';
+
+import '../../controller/home_controller.dart';
+import '../../core/constant/imageAssets.dart';
+
+class HomeGridview extends StatelessWidget {
+  const HomeGridview({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    HomeControllerImpl controller = Get.put(HomeControllerImpl());
+    return Expanded(
+      child: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 44,
+        childAspectRatio: 1, // Ensures square items
+        children: [
+          GridViewItem(
+              onTap: () {
+                controller.goToWithdrawScreen();
+              },
+              imagePath: AppImageassets.withdrawImage,
+              label: 'Withdraw'),
+          GridViewItem(
+              onTap: () {
+                controller.goToTransferScreen();
+              },
+              imagePath: AppImageassets.transferImage,
+              label: 'Transfer'),
+          GridViewItem(
+              onTap: () {
+                controller.goToTransactionScreen();
+              },
+              imagePath: AppImageassets.reportImage,
+              label: 'Transaction'),
+          GridViewItem(
+              onTap: () {
+                controller.goToAccountScreen();
+              },
+              imagePath: AppImageassets.accountImage,
+              label: 'Account'),
+        ],
+      ),
+    );
+  }
+}
